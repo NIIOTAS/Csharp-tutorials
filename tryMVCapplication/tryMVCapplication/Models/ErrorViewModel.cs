6bac7451298e5d915db13d6f2ce5c7e55bde6827
NIIOTAS <niiotasalokome@gmail.com>
2026-02-13 10:49:34 +0000

    scaffold

---
+++ tryMVCapplication/tryMVCapplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tryMVCapplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
